d9ee08180e024fca35c69a1845e0543859db0588
Rutu Raka <60650760+Ruturaka@users.noreply.github.com>
2023-02-01 15:52:10 +0530

    Update OrderedItemsDaoImpl.java

--- src/main/java/com/confluxsys/demo/spring/dao/orderedItmes/OrderedItemsDaoImpl.java
+++ src/main/java/com/confluxsys/demo/spring/dao/orderedItmes/OrderedItemsDaoImpl.java
@@ -1,18 +1,19 @@
-package com.confluxsys.demo.spring.dao.orderedItmes;
+package com.confluxsys.demo.spring.dao.orderedItems;
 
 import com.confluxsys.demo.model.orderedItems.OrderedItems;
-import com.confluxsys.demo.spring.dao.order.OrderDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Types;
+import java.sql.*;
+import java.util.List;
 
+@PropertySource("classpath:database.properties")
 @Component
-public class OrderedItemsDaoImpl implements OrderedItemsDao {
+public class OrderedItemsDaoImpl implements OrderedItemsDao{
     JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -21,14 +22,12 @@ public class OrderedItemsDaoImpl implements OrderedItemsDao {
     }
 
     @Override
-    public int createOrderedItems(OrderedItems orderedItems) {
+    public Integer createOrderedItems(OrderedItems orderedItems) {
         System.out.println("Inside createOrderredItems()");
 
-        String query = "INSERT INTO ordered_items(order_id, dish, quantity, feedback) VALUES (?, ?, ?, ?)  RETURNING item_id";
+        String query="INSERT INTO ordered_items(order_id, dish, quantity, feedback) VALUES (?, ?, ?, ?)  RETURNING item_id";
         System.out.println("hii from ordered_items");
         try {
-           // String query = "INSERT INTO ordered_items(order_id, dish, quantity, feedback) VALUES (?, ?, ?, ?)  RETURNING item_id";
-
             PreparedStatement ps = jdbcTemplate.getDataSource().getConnection().prepareStatement(query);
             ps.setObject(1, orderedItems.getOrder_id(), Types.INTEGER);
             ps.setObject(2, orderedItems.getDish(), Types.VARCHAR);
@@ -40,12 +39,48 @@ public class OrderedItemsDaoImpl implements OrderedItemsDao {
             if (rs != null && rs.next()) {
                 return rs.getInt("item_id");
             }
-        } catch (Exception e) {
+        } catch (Exception e){
             System.out.println(e);
         }
 
         System.out.println("outside try catch");
         return 0;
+    }
+
+    @Override
+    public Integer deleteById(Integer id) {
+        String query="DELETE FROM ordered_items where order_id=?";
+        return jdbcTemplate.update(query,id);
+    }
+
+    @Override
+    public List<OrderedItems> findAll() {
+        try{
+            String query="SELECT * FROM ordered_items";
+            return jdbcTemplate.query(query,BeanPropertyRowMapper.newInstance(OrderedItems.class));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    @Override
+    public List<OrderedItems> findByDish(String dish) {
+        try{
+            String query= "SELECT * FROM ordered_items WHERE dish=?";
+            return jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(OrderedItems.class), dish);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
 
     }
+
+    @Override
+    public Integer deleteAll() {
+        return jdbcTemplate.update("DELETE from ordered_items");
+    }
+
 }
