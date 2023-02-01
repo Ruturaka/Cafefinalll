658b2c98ad0b4a285f2b437e118d80a37346d986
Rutu Raka <60650760+Ruturaka@users.noreply.github.com>
2023-02-01 15:40:30 +0530

    Update Order.java

--- src/main/java/com/confluxsys/demo/model/orders/Order.java
+++ src/main/java/com/confluxsys/demo/model/orders/Order.java
@@ -1,15 +1,18 @@
-package com.confluxsys.demo.model.orders;
+package com.confluxsys.demo.model.Orders;
 
 public class Order {
-
-    Integer order_id;
-    Integer customer_id;
+    Integer order_id; //auto
+    Integer customer_id; // we will take that from customer's table
 
     public Order() {
     }
 
     public Order(Integer customer_id) {
-        this.customer_id = customer_id;
+        this.customer_id=customer_id;
+    }
+
+    public Integer getOrder_id() {
+        return order_id;
     }
 
     public void setOrder_id(Integer order_id) {
