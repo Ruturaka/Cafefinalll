24b97d1b70ef395f77104675e14474ed94f6bc6d
Rutu Raka <60650760+Ruturaka@users.noreply.github.com>
2023-02-01 15:35:59 +0530

    Update Customer.java

--- src/main/java/com/confluxsys/demo/model/customer/Customer.java
+++ src/main/java/com/confluxsys/demo/model/customer/Customer.java
@@ -1,22 +1,26 @@
 package com.confluxsys.demo.model.customer;
 
-import com.confluxsys.demo.bean.OrderItems;
-
-import java.util.List;
-
 public class Customer {
-    private Integer customer_id;
+    Integer customer_id; //auto
     private String name;
     private String email;
     private String mobile;
 
-    public Customer() {
-        System.out.println("inside customer default constructor");
+    private Integer order_id;
 
+    private String dish;
+
+    private Integer quantity;
+
+    private String feedback;
+
+    private Integer bill;
+
+    private Integer revenue;
+
+    public Customer() {
     }
     public Customer(String name, String email, String mobile) {
-        System.out.println("inside customer parameterized constructor");
-
         this.name=name;
         this.email=email;
         this.mobile=mobile;
@@ -53,6 +57,55 @@ public class Customer {
 
     public void setCustomer_id(Integer customer_id) {
         this.customer_id = customer_id;
+    }
+
+
+    public Integer getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Integer order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getDish() {
+        return dish;
+    }
+
+    public void setDish(String dish) {
+        this.dish = dish;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public Integer getBill() {
+        return bill;
+    }
+
+    public void setBill(Integer bill) {
+        this.bill = bill;
+    }
+
+    public Integer getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(Integer revenue) {
+        this.revenue = revenue;
     }
 
     @Override
