babe1ad9fa89ee0a8da6372f63c282f4a88561a5
Rutu Raka <60650760+Ruturaka@users.noreply.github.com>
2023-02-01 15:37:41 +0530

    Update CustomerMapper.java

--- src/main/java/com/confluxsys/demo/model/customer/CustomerMapper.java
+++ src/main/java/com/confluxsys/demo/model/customer/CustomerMapper.java
@@ -12,10 +12,10 @@ public class CustomerMapper implements RowMapper<Customer> {
     @Override
     public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
         Customer customer = new Customer();
-
+        
+        customer.setCustomer_id(rs.getInt("customer_id"));
         customer.setName(rs.getString("name"));  // get customer name from customers table
         customer.setEmail(rs.getString("email")); // get customer name from customers table
-
         customer.setMobile(rs.getString("mobile")); // get order id from orders table
 
         return customer;
