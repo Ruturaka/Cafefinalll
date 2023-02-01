27ec3d4c87f754e765381ed390c0767cd3add22e
Rutu Raka <60650760+Ruturaka@users.noreply.github.com>
2023-02-01 15:31:49 +0530

    Update CustomerController.java

--- src/main/java/com/confluxsys/demo/controller/CustomerController.java
+++ src/main/java/com/confluxsys/demo/controller/CustomerController.java
@@ -1,129 +1,102 @@
 package com.confluxsys.demo.controller;
 
-import com.confluxsys.demo.bean.CustomerOrder;
-import com.confluxsys.demo.bean.OrderItems;
 import com.confluxsys.demo.model.customer.Customer;
-import com.confluxsys.demo.model.orderedItems.OrderedItems;
-import com.confluxsys.demo.model.orders.Order;
 import com.confluxsys.demo.spring.dao.customer.CustomerDao;
-import com.confluxsys.demo.spring.dao.order.OrderDao;
-import com.confluxsys.demo.spring.dao.orderedItmes.OrderedItemsDao;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8088")
+@CrossOrigin(origins = "http://localhost:9999")
 @RestController
 @RequestMapping("/capi")
-
 public class CustomerController {
 
     @Autowired
     CustomerDao customerDao;
 
-    @Autowired
-    OrderDao orderDao;
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-    @Autowired
-    OrderedItemsDao orderedItemsDao;
-
-    @PostMapping("/customers")
-    public ResponseEntity<String> createCustomerOrder(@RequestBody CustomerOrder body) {
-
-        try{
-            Customer cust = new Customer();
-            cust.setName(body.getName());
-            cust.setEmail(body.getEmail());
-            cust.setMobile(body.getMobile());
-            Integer customer_id= customerDao.addCustomer(cust);
-            System.out.println("customer_id="+customer_id);
-
-            Order o=new Order(customer_id);
-            Integer order_id= orderDao.createOrder(o);
-            System.out.println("order_id"+order_id);
-
-            OrderedItems oi= new OrderedItems(order_id);
-            List<OrderItems> orderItemsList = body.getOrderItemsList();
-
-
-            Integer item_id;
-            //item_id= orderedItemsDao.createOrderedItems(oi);
-            for (OrderItems item : orderItemsList) {
-                oi.setOrder_id(order_id);
-                oi.setDish(item.getDish());
-                oi.setQuantity(item.getQuantity());
-                oi.setFeedback(item.getFeedback());
-                System.out.println("order - " + oi);
-                item_id= orderedItemsDao.createOrderedItems(oi);
-                System.out.println("item_id: "+item_id);
-            }
-            System.out.println("Done!!");
-
-            return new ResponseEntity<>("CustomerOrder was created successfully. ", HttpStatus.CREATED);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-   /* @Autowired
-    CustomerDao customerDao;
-
-    @Autowired
-    OrderDao orderDao;
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-    @Autowired
-    OrderedItemsDao orderedItemsDao;
-
-    @PostMapping("/customers")
-    public ResponseEntity<String> createCustomerOrder(@RequestBody CustomerOrder body) {
+    @GetMapping("/customer")
+    public ResponseEntity<List<Customer>> getAllCustomers(@RequestParam(required = false) String name){
         try {
+            List<Customer> customers = new ArrayList<Customer>();
 
-            Customer cust = new Customer();
-            cust.setName(body.getName());
-            cust.setEmail(body.getEmail());
-            cust.setMobile(body.getMobile());
-            Integer customer_id= customerDao.createCustomer(cust);
-            System.out.println("customer_id="+customer_id);
-
-            Order o=new Order(customer_id);
-            Integer order_id= orderDao.createOrder(o);
-            System.out.println("order_id"+order_id);
-
-
-            //orderedItemsDao.addOrderItems(orderId, orderItemsList);
-
-
-           // Integer order_id= orderDao.createOrder(o);
-            //System.out.println("order_id for orders- "+order_id);
-
-           /* OrderedItems oi= new OrderedItems(order_id);
-            List<OrderItems> orderItemsList = body.getOrderItemsList();
-
-            for (OrderItems item : orderItemsList) {
-                oi.setOrder_id(order_id);
-                oi.setDish(item.getDish());
-                oi.setQuantity(item.getQuantity());
-                oi.setFeedback(item.getFeedback());
-                System.out.println("order - "+oi);
-                orderedItemsDao.createOrderedItems(oi);
+            if (name == null)
+                customerDao.findAll().forEach(customers::add);
+            else
+                customerDao.findByNameContaining(name).forEach(customers::add);
+            if (customers.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-
-
-            return new ResponseEntity<>("CustomerOrder was created successfully. ", HttpStatus.CREATED);
-
+            return new ResponseEntity<>(customers, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-*/
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Customer> getDishById(@PathVariable("id") Integer id) {
+        Customer customer = customerDao.findById(id);
+
+        if (customer != null) {
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<String> createCustomer(@RequestBody Customer customer) {
+        try {
+            customerDao.save(new Customer(customer.getName(), customer.getEmail(), customer.getMobile()));
+            return new ResponseEntity<>("Customer created successfully.", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<String> updateCustomer(@PathVariable("id") Integer id, @RequestBody Customer customer) {
+        Customer _customer = customerDao.findById(id);
+
+        if (_customer != null) {
+            _customer.setName(customer.getName());
+            _customer.setEmail(customer.getEmail());
+            _customer.setMobile(customer.getMobile());
+
+            customerDao.update(_customer);
+            return new ResponseEntity<>("Customer updated successfully.", HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>("Cannot find Customer with id=" + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/customer/{customer_id}")
+    public ResponseEntity<String> deleteCustomerById(@PathVariable("customer_id") Integer customer_id) {
+        try {
+            int result = customerDao.deleteById(customer_id);
+            if (result == 0) {
+                return new ResponseEntity<>("Cannot find Customer with id=" + customer_id, HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Customer deleted successfully.", HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>("Cannot delete customer.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/customer")
+    public ResponseEntity<String> deleteAll() {
+        try {
+            int numRows = customerDao.deleteAll();
+            return new ResponseEntity<>("Deleted" + numRows + "Customer(s) successfully", HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>("Cannot delete customers", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
